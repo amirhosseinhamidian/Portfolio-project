7992030d5d11c064123aeec0b763b152b2d0bca3
amir <amirhossein611@gmail.com>
2024-07-28 19:00:43 +0330

    home & about & services & resume

--- script/app.js
+++ script/app.js
@@ -1,8 +1,19 @@
 const navToggleIcon = document.querySelector(".nav__toggle-icon")
 const menu = document.querySelector(".menu")
 const cover = document.querySelector(".cover")
+const resumeListItems = document.querySelectorAll('.resume-list__item')
 navToggleIcon.addEventListener("click", function() {
     this.classList.toggle("nav__toggle-icon--open")
     menu.classList.toggle("menu--open")
     cover.classList.toggle("cover--show")
+})
+
+resumeListItems.forEach(item=>{
+    item.addEventListener('click',function(){
+        document.querySelector('.resume-list__item--active').classList.remove('resume-list__item--active')
+        document.querySelector('.resume-content--show').classList.remove('resume-content--show')
+        this.classList.add('resume-list__item--active')
+        let contentId = this.getAttribute('data-content-id')
+        document.querySelector(contentId).classList.add('resume-content--show')
+    })
 })
